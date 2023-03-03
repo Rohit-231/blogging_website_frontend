f743b24a2613702aeb8dc9c79dab2761ab6507af
Rohit-231 <pal8035@gmail.com>
2023-03-03 16:00:58 +0530

    changes for login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl,Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/app/model/user.model';
+import { BlogService } from 'src/app/service/blog.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm:FormGroup;
+  email:string;
+  password:string;
+  user:User
+  constructor(private router:Router, private blogService:BlogService) { }
 
   ngOnInit(): void {
+    this.loginForm=new FormGroup({
+      email:new FormControl('',[Validators.required]),
+      password:new FormControl('',[Validators.required])
+    })
+  }
+
+  onFormSubmit(){
+    console.log(this.loginForm.value);
+    this.email=this.loginForm.value.email;
+    this.password=this.loginForm.value.password
+
+    this.blogService.login(this.email,this.password).subscribe(data=>{
+      console.log(data);
+      
+      this.user=data['data']['user'];
+      console.log(this.user);
+      
+    })
+    
   }
 
 }
--- src/app/service/blog.service.ts
+++ src/app/service/blog.service.ts
@@ -1,7 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Blog } from '../model/blog.model';
 import {Observable} from 'rxjs';
+import { User } from '../model/user.model';
+import { Buffer } from 'buffer';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +11,23 @@ import {Observable} from 'rxjs';
 export class BlogService {
 
   allblogApi='https://cloudy-wig-seal.cyclic.app/api/v1/blogs';
-
+  loginApi='https://cloudy-wig-seal.cyclic.app/api/v1/users/login'
 
   constructor(private http:HttpClient) { }
 
   getAllBlog():Observable<Blog[]>{
     return this.http.get<Blog[]>(this.allblogApi);
+  }
+  login(email:string,password:string):Observable<User>{
+    let encodedString = email+':'+password;
+    console.log(encodedString);
+    
+    let httpOptions={
+      headers: new HttpHeaders({
+        'Content-type':'application/json',
+        'Authorization':'basic'+encodedString
+      })
+    };
+    return this.http.post<User>(this.loginApi,httpOptions);
   }
 }
